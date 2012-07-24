ab392a8f98c05e8466c5f343002afac80267ce8d
snipplets <leeroyjenkins30@gmail.com>
2012-07-24 15:31:37 +1000

    Finished Problem 61

--- Python/Problem61.py
+++ Python/Problem61.py
@@ -1,15 +1,3 @@
-#generates a sequence of numbers in a range
-def genSeq(min,max,mode):
-	out = []
-	seq = 1
-	i = 1
-	while seq < max:
-		if seq > min:
-			out.append(seq)
-		i+=1
-		seq = operation(i,mode)
-	return out
-
 #Returns the a number from the specified sequence
 def operation(i,mode):
 	if mode == 3:
@@ -24,14 +12,32 @@ def operation(i,mode):
 		return i*(5*i-3)/2
 	if mode == 8:
 		return i*(3*i-2)
-def checkCyclic(nums):
 	
-#Generate all relevent sequences
-tri = genSeq(1000,10000,3)
-sqr = genSeq(1000,10000,4)
-pen = genSeq(1000,10000,5)
-hex = genSeq(1000,10000,6)
-hep = genSeq(1000,10000,7)
-oct = genSeq(1000,10000,8)
+#Generate all relevent sequences, store in (type,num) pair
+seq = []
+for x in range(6):
+	for y in range(141): #last number such that triangle(y)<10000
+		if 1000 <= operation(y,x+3) <= 10000:
+			seq.append((x+3,operation(y,x+3)))
 
-
+#Find cycles and sum
+for pair in seq: 
+	for pair1 in seq:
+		checked = []
+		if pair[1] % 100 == pair1[1] / 100 and pair[0] != pair1[0]:
+			checked.append(pair[0])
+			checked.append(pair1[0])
+			for pair2 in seq:
+				if pair1[1] % 100 == pair2[1] / 100 and pair2[0] not in checked:
+					checked.append(pair2[0])
+					for pair3 in seq:
+						if pair2[1] % 100 == pair3[1] / 100 and pair3[0] not in checked:
+							checked.append(pair3[0])
+							for pair4 in seq:
+								if pair3[1] % 100 == pair4[1] / 100 and pair4[0] not in checked:
+									checked.append(pair4[0])
+									for pair5 in seq:
+										if pair4[1] % 100 == pair5[1] / 100 and pair5[1] % 100 == pair[1]/100 and pair5[0] not in checked:
+											checked.append(pair5[0])
+											print pair,pair1,pair2,pair3,pair4,pair5
+											print pair[1]+pair1[1]+pair2[1]+pair3[1]+pair4[1]+pair5[1]
